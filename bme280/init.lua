5ce22b26e525b247d68495cee90dde0d2cc688fe
chain01 <15863338830@163.com>
2019-03-18 09:40:51 +0800

    气压上传

--- bme280/init.lua
+++ bme280/init.lua
@@ -14,4 +14,15 @@ tmr.alarm(3,10000,tmr.ALARM_AUTO,function()
 	if H~=nil    then
 		print(string.format("humidity=%d.%03d%%", H/1000, H%1000))
 	end
+	pin = 1
+	status, temp, humi, temp_dec, humi_dec = dht.read(pin)
+	if status == dht.OK then
+		-- Float firmware using this example
+		print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+
+	elseif status == dht.ERROR_CHECKSUM then
+		print( "DHT Checksum error." )
+	elseif status == dht.ERROR_TIMEOUT then
+		print( "DHT timed out." )
+end
 end)
